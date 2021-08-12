c74c7112b64622e68ba4c8e75660ef205f27fac2
sumaharika <87772497+sumaharika@users.noreply.github.com>
2021-08-12 07:26:01 +0530

    Add files via upload

---
+++ SpringBoot/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.ust.example;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+
+public class SpringBootApplication {
+ public static void main(String array[])
+ {
+	 SpringApplication.run(SpringBootApplication.class,array);
+ }
+}
